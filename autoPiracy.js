bfc7a261a0d712ae359b4d47f6241c183362bc3c
Jethro Guce <seraphz64@gmail.com>
2018-10-30 18:22:35 +0800

    Update autoPiracy.js

--- autoPiracy.js
+++ autoPiracy.js
@@ -1,13 +1,23 @@
+var counter = [];
 function autoPiracy(){
+  function msToMMSS(millis) {
+    var minutes = Math.floor(millis / 60000);
+    var seconds = ((millis % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+  }
+
   if (document.getElementsByClassName('captchaImage').length > 0) {
+    counter.push({'now': new Date(), 'message': 'capcha validation'});
     alert('capcha');
   } else {
     var capture = document.getElementsByClassName('capture');
     if (capture.length > 0){
       capture[0].click();
-      var next =  Math.round(Math.random() * 3000) + 180000;
+      var next =  Math.round(Math.random() * 10000) + 170000;
+      counter.push({'now': new Date(), 'next': msToMMSS(next)});
       setTimeout(autoPiracy, next);
     } else {
+      counter.push({'now': new Date(), 'message': 'cannot find capture button'});
       alert('Cannot Find Capture button, stopping script');
     }
   }
